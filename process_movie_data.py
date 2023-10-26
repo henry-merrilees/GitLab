8b72aa6014695323b6ca7dba9ad0690fbf6cc1ab
Henry Merrilees <henry@merrile.es>
2023-10-26 10:58:26 -0700

    top_ten

--- process_movie_data.py
+++ process_movie_data.py
@@ -3,7 +3,7 @@
 import csv
 
 
-def find_top_N(filename, n=5):
+def find_top_N(filename, n=10):
     """Finds the top n highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Output: None
@@ -22,10 +22,17 @@ def find_top_N(filename, n=5):
     # Sort data and get top 5
     gross_sort = lambda x : x["Gross"]
     rows.sort(key=gross_sort)
+<<<<<<< HEAD
     top_n = rows[:-n-1:-1]
 
     # Print out results
     for i, row in enumerate(top_n):
+=======
+    top_ten = rows[:-11:-1]
+
+    # Print out results
+    for i, row in enumerate(top_10):
+>>>>>>> 8b7440a (top_ten)
         print("{ind}. {row[Title]} ({row[Year]}) - ${row[Gross]:,d}".format(
             ind=i+1,
             row=row))
@@ -35,4 +42,8 @@ def find_top_N(filename, n=5):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
+<<<<<<< HEAD
     find_top_N("Movies_gross_rating.csv")
+=======
+    find_top_10("Movies_gross_rating.csv")
+>>>>>>> 8b7440a (top_ten)
